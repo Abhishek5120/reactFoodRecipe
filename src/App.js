60dc84fd6ab3a87e54cc431e932563865f85ade2
Abhishek5120 <abhishekverma7155@gmail.com>
2024-11-11 22:28:46 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const BASE_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [ingredient, setIngredient] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+  const [selectedIngredient, setSelectedIngredient] = useState(null);
+
+  function chooseIngredient(idx) {
+    const newIngredient = ingredient[idx];
+    setInput(newIngredient);
+    setSelectedIngredient(newIngredient);
+    findDish(newIngredient);
+  }
+
+
+  function findDish(newIngredient) {
+    const searchIng = newIngredient || input;
+    if (searchIng.length === 0) {
+      alert("Please provide/choose an Ingredient first");
+      return;
+    }
+
+    setIsLoading(true);
+    setNoResults(false);
+    // Fetching Meal List on Search button click
+    fetch(BASE_URL + searchIng)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.meals) {
+          setData(data.meals);
+        } else {
+          setData([]);
+          setNoResults(true); // Set noResults to true if no meals are found
+        }
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    /*
+      fetching ingredient List in first rendering 
+      to give user some options to choose from
+     */
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?i=list")
+      .then((res) => res.json())
+      .then((data) => {
+        const ingredientArr = data.meals.filter((ing, idx) => {
+          if (idx < 30) return ing;
+        });
+        setIngredient(ingredientArr.map((ing, idx) => ing.strIngredient));
+      })
+      .catch((err) => console.log(err));
+    // .finally(() => console.log(ingredient));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <nav>
+        <input
+          className="searchBox"
+          value={input}
+          placeholder="Enter the ingredients..."
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="searchButton" onClick={() => findDish(input)}>
+          Search
+        </button>
+      </nav>
       </header>
+      <p>
+        Search a meal by it's main ingredient! Or choose one from the given list
+      </p>
+      <div className="ingredient">
+        <ul className="ingredientUnorderedList">
+          {ingredient.map((ing, idx) => (
+            <li
+              key={ing}
+              className={`ingredients ${
+                selectedIngredient === ing ? "selected" : ""
+              }`}
+              onClick={() => chooseIngredient(idx)}
+            >
+              {ing}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {isLoading && <Loader />}
+      {/* Display "No Recipe Found" message if no results and not loading */}
+      {!isLoading && noResults && <p>No Recipe Found</p>}
+      {data.length !== 0 && !noResults ? <MealList meals={data} /> : null}
     </div>
   );
 }
 
-export default App;
+function Loader() {
+  return <div>Loading Meals from the ingredients provided...</div>;
+}
+
+function MealList({ meals }) {
+  return (
+    <div className="mealList">
+      {meals.map((meal) => (
+        <Meal
+          key={meal.idMeal}
+          name={meal.strMeal}
+          imgSrc={meal.strMealThumb}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Meal({ name, imgSrc }) {
+  return (
+    <div className="recipe">
+      <img src={imgSrc} height="180px" width="180px" />
+      <p>{name}</p>
+    </div>
+  );
+}
+
